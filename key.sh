e2841d5ab66fb956b7c1e9c9165b03321b78b5a5
dleovl <95427722+dleovl@users.noreply.github.com>
2024-06-24 02:43:26 -0400

    final

--- key.sh
+++ key.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-clear
-echo -e "Please put your device into pwned DFU mode with gaster pwn.\nThis script may not be compatible with i(Pad)OS 18.\nRequirements: Criptam, irecovery, pzb, img4, pyimg4\nMade by @dleovl"
+echo -e "\nPlease put your device into pwned DFU mode with gaster pwn.\nThis script may not be compatible with i(Pad)OS 18.\nRequirements: Criptam, irecovery, pzb, img4, pyimg4\nMade by @dleovl\n"
 
 deviceinfo=$(irecovery -q)
 deviceid=$(echo "$deviceinfo" | awk '/PRODUCT/ {print $NF}')
 boardconfig=$(echo "$deviceinfo" | awk '/MODEL/ {print $NF}')
-rm -f *.im4p *.txt *.dec fw.json BuildManifest.plist 2>/dev/null
-wget https://api.ipsw.me/v4/device/${deviceid} -O fw.json > /dev/null 2>&1
-echo -e "\nDetected ${deviceid} ${boardconfig}\n"
+rm -f *.im4p *.txt *.dec fw.json BuildManifest.plist
+wget https://api.ipsw.me/v4/device/${deviceid} -O fw.json
+echo -e "Detected ${deviceid} ${boardconfig}\n"
 
 echo -e "Enter .ipsw URL and press enter:\n"
 read ipswurl
@@ -16,21 +15,22 @@ filename=$(basename "$ipswurl")
 version=$(echo "$filename" | awk -F '_' '{print $(NF-2)}')
 buildid=$(echo "$filename" | awk -F '_' '{print $(NF-1)}')
 
-pzb --list "$ipswurl" > list.txt 2>&1
+pzb --list "$ipswurl" > list.txt
 open -e list.txt
 
 echo -e "\nEnter codename for ${buildid} (this can be acquired from Wikipedia under "Release history")\n"
 read codename
 
-clear
-echo -e "Made by @dleovl\n\nUsing $ipswurl\n\nPlease wait..."
+echo -e "\nDownloading components...\n"
 
-pzb --get "BuildManifest.plist" "$ipswurl" > /dev/null
-pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/iBSS[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl" > /dev/null
-pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/iBEC[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl" > /dev/null
-pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/LLB[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl" > /dev/null
-pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/iBoot[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl" > /dev/null
-pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/sep-firmware[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl" > /dev/null
+pzb --get "BuildManifest.plist" "$ipswurl"
+pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/iBSS[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl"
+pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/iBEC[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl"
+pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/LLB[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl"
+pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/iBoot[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl"
+pzb --get $(awk "/""${boardconfig}""/{x=1}x&&/sep-firmware[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1) "$ipswurl"
+
+echo -e "\nObtaining keys...\n"
 
 criptam --build-id "$buildid" --device-identifier "$deviceid" > keys.txt
 
@@ -47,10 +47,12 @@ ibootiv=$(awk '/iBoot IV:/ {print $NF}' keys.txt)
 ibootkey=$(awk '/iBoot Key:/ {print $NF}' keys.txt)
 ibootfilename=$(basename $(awk "/${boardconfig}/{x=1}x&&/iBoot[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1))
 
-img4 -i ${ibssfilename} -o iBSS.dec -k ${ibssiv}${ibsskey} > /dev/null
-img4 -i ${ibecfilename} -o iBEC.dec -k ${ibeciv}${ibeckey} > /dev/null
-img4 -i ${ibootfilename} -o iBoot.dec -k ${ibootiv}${ibootkey} > /dev/null
-img4 -i ${llbfilename} -o LLB.dec -k ${llbiv}${llbkey} > /dev/null
+echo -e "Decrypting components...\n"
+
+img4 -i ${ibssfilename} -o iBSS.dec -k ${ibssiv}${ibsskey}
+img4 -i ${ibecfilename} -o iBEC.dec -k ${ibeciv}${ibeckey}
+img4 -i ${ibootfilename} -o iBoot.dec -k ${ibootiv}${ibootkey}
+img4 -i ${llbfilename} -o LLB.dec -k ${llbiv}${llbkey}
 open -e iBSS.dec iBEC.dec iBoot.dec LLB.dec
 
 sepfwfilename=$(basename $(awk "/${boardconfig}/{x=1}x&&/sep-firmware[.]/{print;exit}" BuildManifest.plist | grep '<string>' |cut -d\> -f2 |cut -d\< -f1))
@@ -95,4 +97,4 @@ echo "${content}" > "wiki/${codename}_${buildid}_(${deviceid}).txt"
 open -e "wiki/${codename}_${buildid}_(${deviceid}).txt"
 rm -f *.im4p
 
-echo "Finished. Check for legible text in iBSS/iBEC/iBoot/LLB (they have been opened in TextEdit). SEP firmware is not decrypted. Only contribute if you are entirely confident these are correct."
+echo -e "\nFinished, please make sure there are no errors. Check for legible text in iBSS/iBEC/iBoot/LLB (they have been opened in TextEdit).\nSEP firmware is not decrypted. Only contribute if you are entirely confident these are correct.\n\nYour file has been saved as wiki/${codename}_${buildid}_(${deviceid}).txt\n"
